4a59b99dfbeadbbcbc071bd1224527b0727d5a5f
gbriyad <gbriyad@gmail.com>
2023-07-19 21:45:23 +0600

    try

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage", '.bundle', 'public/uploads'
