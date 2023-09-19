e47f23102c038bdf30f05f50f794268a78c2e551
gbriyad <gbriyad@gmail.com>
2023-09-19 22:08:29 +0600

    update UI

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,4 +6,9 @@ module ProductsHelper
   def show_product_change_rate(product)
     "#{'+' if product.change_rate > 0.0}#{number_with_precision(product.change_rate, precision: 2)}%"
   end
+
+  def show_growth_icon(product)
+    image_file_name = product.change_rate > 0.0 ? 'positive-growth.svg' : 'negative-growth.svg'
+    image_tag image_file_name, class: 'img-fluid'
+  end
 end
