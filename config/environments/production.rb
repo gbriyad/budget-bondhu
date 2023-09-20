c8f1032e66ad4426f10173755f7c94ee24ac0b48
gbriyad <gbriyad@gmail.com>
2023-09-21 02:21:45 +0600

    update favicon, enable logging for production cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "product_price_analytics_production"
 
   config.action_mailer.perform_caching = false
+  config.action_controller.enable_fragment_cache_logging = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
