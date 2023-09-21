1781cd44d5bad626ad5e6e190dd3d2952f0b9a55
gbriyad <gbriyad@gmail.com>
2023-09-21 14:50:29 +0600

    fix cache issue (product timestamp not updating when new price was added)

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,6 @@
 class Price < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, touch: true
+
   enum time_period: {last_1_week: 1.week, last_2_weeks: 2.weeks,
                      last_1_month: 1.month, last_3_months: 3.months, last_6_months: 6.months,
                      last_1_year: 1.year, last_2_years: 2.years}
