720a7e73af45eea9f57bde963949e44fc9720426
gbriyad <gbriyad@gmail.com>
2023-10-01 00:15:54 +0600

    update js asset for bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,7 +3,7 @@
 pin "application", preload: true
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.1/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "bootstrap", to: "bootstrap.min.js"
+pin "@popperjs/core", to: "popper.js"
 
 pin_all_from "app/javascript/components", under: "components"
